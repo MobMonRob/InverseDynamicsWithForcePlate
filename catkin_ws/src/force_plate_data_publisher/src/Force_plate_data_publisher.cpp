c283476bc7887f9f83a64a98ed47d493e1d74984
deralbert <kurt.goedel@protonmail.com>
2023-07-13 01:44:57 +0200

    Changes for marker acquisition.

--- catkin_ws/src/force_plate_data_publisher/src/Force_plate_data_publisher.cpp
+++ catkin_ws/src/force_plate_data_publisher/src/Force_plate_data_publisher.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <limits>
 
-#include "force_plate_data_publisher/Force_plate_data.h"
-#include "force_plate_data_acquisition/ForcePlateDataAcquisition.hpp"
+#include "force_plate_data_publisher/Vicon_compound_data.h"
+#include "force_plate_data_acquisition/ViconDataAcquisition.hpp"
 
 using namespace force_plate_data_publisher;
 using namespace Acquisition;
@@ -15,14 +15,20 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Force_plate_data_publisher");
     ros::NodeHandle n;
-    ros::Publisher publisher = n.advertise<Force_plate_data>("Force_plate_data", 1000);
+    ros::Publisher publisher = n.advertise<Vicon_compound_data>("Vicon_compound_data", 1000);
 
-    ForcePlateDataAcquisition forcePlateDataAcquisition;
+    ViconDataAcquisition viconDataAcquisition(ViconDataAcquisition::create());
 
     long prevFrameNum = std::numeric_limits<long>::max() - 1;
     while (ros::ok())
     {
-        ForcePlateDataFrame dataFrame = forcePlateDataAcquisition.grabDirect(ForcePlateDataAcquisition::amti2);
+        Vicon_compound_data msg_Vicon_compound_data;
+
+        viconDataAcquisition.waitForFrame();
+
+        //////////////////////////////////////////////////////////////
+
+        ForcePlateDataFrame dataFrame = viconDataAcquisition.grabForcePlataDataFrame(ViconDataAcquisition::amti2);
 
         if (dataFrame.frameNumber > prevFrameNum + 1)
         {
@@ -32,17 +38,37 @@ int main(int argc, char **argv)
 
         for (const ForcePlateData &dataPoint : dataFrame.forcePlateDataVector)
         {
-            Force_plate_data msg;
+            Force_plate_data msg_Force_plate_data;
 
-            msg.f_x = dataPoint.fX;
-            msg.f_y = dataPoint.fY;
-            msg.f_z = dataPoint.fZ;
-            msg.m_x = dataPoint.mX;
-            msg.m_y = dataPoint.mY;
-            msg.m_z = dataPoint.mZ;
+            msg_Force_plate_data.f_x = dataPoint.fX;
+            msg_Force_plate_data.f_y = dataPoint.fY;
+            msg_Force_plate_data.f_z = dataPoint.fZ;
+            msg_Force_plate_data.m_x = dataPoint.mX;
+            msg_Force_plate_data.m_y = dataPoint.mY;
+            msg_Force_plate_data.m_z = dataPoint.mZ;
 
-            publisher.publish(msg);
+            msg_Vicon_compound_data.force_plate_data.push_back(std::move(msg_Force_plate_data));
         }
+
+        //////////////////////////////////////////////////////////////
+
+        std::vector<MarkerGlobalTranslationData> markerGlobalTranslationDataVector = viconDataAcquisition.grabMarkerGlobalTranslation();
+
+        for (const MarkerGlobalTranslationData& markerGlobalTranslationData : markerGlobalTranslationDataVector)
+        {
+            Marker_global_translation msg_Marker_global_translation;
+
+            msg_Marker_global_translation.x = markerGlobalTranslationData.x;
+            msg_Marker_global_translation.y = markerGlobalTranslationData.y;
+            msg_Marker_global_translation.z = markerGlobalTranslationData.z;
+            msg_Marker_global_translation.occluded = markerGlobalTranslationData.occluded;
+
+            msg_Vicon_compound_data.marker_global_translation.push_back(std::move(msg_Marker_global_translation));
+        }
+
+        //////////////////////////////////////////////////////////////
+
+        publisher.publish(msg_Vicon_compound_data);
 
         ros::spinOnce();
     }
