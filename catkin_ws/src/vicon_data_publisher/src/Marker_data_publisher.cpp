3135e363619b148cd88539b980f0206f72348b46
deralbert <kurt.goedel@protonmail.com>
2023-07-16 17:58:42 +0200

    Added time measurement.

--- catkin_ws/src/vicon_data_publisher/src/Marker_data_publisher.cpp
+++ catkin_ws/src/vicon_data_publisher/src/Marker_data_publisher.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <limits>
+#include <chrono>
+using namespace std::chrono;
 
 #include "vicon_data_publisher/Marker_global_translation.h"
 #include "vicon_data_acquisition/MarkerDataAcquisition.hpp"
@@ -27,11 +29,21 @@ int main(int argc, char **argv)
 
     while (ros::ok())
     {
+        auto start = high_resolution_clock::now();
         const uint frameNumber = markerDataAcquisition.getFrame();
+        auto stop = high_resolution_clock::now();
+
+        if ((frameNumber % 240) == 0)
+        {
+            auto duration = duration_cast<microseconds>(stop - start);
+            ROS_INFO("getFrame() needed %ld microseconds. (More = better)", duration.count());
+        }
 
         if (frameNumber > prevFrameNum + 1)
         {
             ROS_WARN("Lost frames from %d to inclusive %d!", prevFrameNum + 1, frameNumber - 1);
+            auto duration = duration_cast<microseconds>(stop - start);
+            ROS_WARN("getFrame() needed %ld microseconds. (More = better)", duration.count());
         }
         prevFrameNum = frameNumber;
 
