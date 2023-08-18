22a985d38978a431bb5929c4bdde30fdf04736f6
deralbert <kurt.goedel@protonmail.com>
2023-08-18 16:50:46 +0200

    Added normed PABs.

--- Scripts/Pipelines/Inverse_dynamics.py
+++ Scripts/Pipelines/Inverse_dynamics.py
@@ -5,17 +5,23 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))  # nopep8
 from Common.Rosbag_extractor import RosbagExtractor, IndexedBagMsgs, BagMsgs, BagMessage
 from Common.Inverse_dynamics_node import Inverse_dynamics_node
 from Common.Bland_Altman_Plot import BAP_config, BAP_set, generate_bland_altman_plot
-from itertools import cycle
+from itertools import cycle, product
 from Common.Ros_msg_types.data_transformation.msg import Joints_spatial_force, Spatial_force
+from typing import Tuple
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+from numpy.linalg import norm
 
 
 def execute():
     rootDir: str = os.path.abspath(f"{SCRIPT_DIR}/../..")
     dataDir: str = f"{rootDir}/Data/"
-    relativeBagPath: str = f"2023_08_04_ur5e_dynamic/end_position_to_dynamic_random_2023-08-04-19-18-34.bag"
+    # relativeBagPath: str = f"2023_08_04_ur5e_dynamic/end_position_to_dynamic_random_2023-08-04-19-18-34.bag"
+    relativeBagPath: str = f"2023_08_04_ur5e_dynamic/start_position_to_dynamic_random_2023-08-04-19-08-59.bag"
     bagPath: str = f"{dataDir}{relativeBagPath}"
     # bagPath: str = f"{dataDir}2023_08_04_ur5e_static/static_south_2023-08-04-18-20-12.bag"
-    plotSaveDir: str = f"{rootDir}/Plots/Inverse_dynamics/force_to_joint/{relativeBagPath}/"
+    plotSaveDir: str = f"{rootDir}/Plots/Inverse_dynamics/{relativeBagPath}/"
 
     # TODO: sma austauschen durch 1euro
     topic_fp: str = "/Force_plate_data"
@@ -30,7 +36,86 @@ def execute():
 
     joints_spatial_force_list: list[Joints_spatial_force] = create_joints_spatial_force_list(topic_fp, topic_jp, msgs_compound_sorted)
 
-    force_to_joint_plot(joints_spatial_force_list, plotSaveDir)
+    # force_to_joint_plot(joints_spatial_force_list, plotSaveDir)
+
+    bu_df, td_df = create_bu_td_forces_joints_to_values(joints_spatial_force_list)
+    norm_to_joint_plot(bu_df, td_df, plotSaveDir)
+
+    return
+
+
+def create_bu_td_forces_joints_to_values(joints_spatial_force_list: "list[Joints_spatial_force]") -> Tuple[pd.DataFrame, pd.DataFrame]:
+    # Alternative wäre, Subklasse von dict zu machen, die ein Schlüssel einfügt, wenn nicht vorhanden.
+    bu_forces_joints_to_values: dict[(int, int), list[float]] = {(i, j): [] for i, j in product(range(6), range(6))}
+    td_forces_joints_to_values: dict[(int, int), list[float]] = {(i, j): [] for i, j in product(range(6), range(6))}
+    for joints_spatial_force in joints_spatial_force_list:
+        joints_forces_bottom_up: list[Spatial_force] = joints_spatial_force.joints_bottom_up
+        joints_forces_top_down = joints_spatial_force.joints_top_down
+        for joint_index in range(6):
+            joint_forces_bottom_up: list[float] = joints_forces_bottom_up[joint_index].m_xyz__f_xyz
+            joint_forces_top_down: list[float] = joints_forces_top_down[joint_index].m_xyz__f_xyz
+            for force_component_index in range(6):
+                key: Tuple[int, int] = (force_component_index, joint_index)
+
+                joint_force_bottom_up: float = joint_forces_bottom_up[force_component_index]
+                joint_force_top_down: float = joint_forces_top_down[force_component_index]
+
+                bu_forces_joints_to_values[key].append(joint_force_bottom_up)
+                td_forces_joints_to_values[key].append(joint_force_top_down)
+
+    # Alternative, von columns nach multiindex: columns machen und dann pivot_table
+    # Andersherum, von multiindex nach columns: unstack
+    size = len(joints_spatial_force_list)
+    bu = np.concatenate(list(bu_forces_joints_to_values.values()))
+    td = np.concatenate(list(td_forces_joints_to_values.values()))
+    mi = pd.MultiIndex.from_product(iterables=[range(6), range(6), range(size)], names=["spatial_force", "joint", "index"])
+    bu_df = pd.DataFrame(data=bu, index=mi, columns=["value"])
+    td_df = pd.DataFrame(data=td, index=mi, columns=["value"])
+    return bu_df, td_df
+
+
+def norm_m_f(df: DataFrame) -> DataFrame:
+    df = df.unstack(level="spatial_force")
+    df = df.apply(func=lambda row: (norm(row[0:3]), norm(row[3:6])), axis="columns", result_type="expand")
+    df.columns = ["m", "f"]
+    return df
+
+
+def norm_to_joint_plot(bu_df: DataFrame, td_df: DataFrame, plotSaveDir: str):
+    # bu_forces_joints_to_values
+
+    # Alternativ hätte man das auch mit einem ros node machen können.
+    # Bzw. über den Kern des nodes drüber iterieren, ähnlich wie in create_joints_spatial_force_list.
+    # Aber das war gute pandas Übung.
+    bu_df_normed_m_f = norm_m_f(bu_df)
+    td_df_normed_m_f = norm_m_f(td_df)
+
+    bu_df_normed_m_f = bu_df_normed_m_f.unstack(level="joint")
+    td_df_normed_m_f = td_df_normed_m_f.unstack(level="joint")
+
+    bu_joint_to_normed_m_values: list[list[float]] = [bu_df_normed_m_f[("m", i)].values.tolist() for i in range(6)]
+    bu_joint_to_normed_f_values: list[list[float]] = [bu_df_normed_m_f[("f", i)].values.tolist() for i in range(6)]
+    td_joint_to_normed_m_values: list[list[float]] = [td_df_normed_m_f[("m", i)].values.tolist() for i in range(6)]
+    td_joint_to_normed_f_values: list[list[float]] = [td_df_normed_m_f[("f", i)].values.tolist() for i in range(6)]
+
+    sets_m: list[BAP_set] = [BAP_set(x1=bu_joint_to_normed_m_values[i], x2=td_joint_to_normed_m_values[i]) for i in range(6)]
+    sets_f: list[BAP_set] = [BAP_set(x1=bu_joint_to_normed_f_values[i], x2=td_joint_to_normed_f_values[i]) for i in range(6)]
+
+    colors_joints = list()
+    colors_joints.extend(["b", "g", "r", "c", "m", "y"])
+
+    dataName1 = f"bottom_up"
+    dataName2 = f"top_down"
+    plotSaveDir = f"{plotSaveDir}norm_to_joint/"
+
+    config_m: BAP_config = BAP_config(sets=sets_m, colors=iter(colors_joints), dataName1=dataName1, dataName2=dataName2,
+                                      units="[Nm]", additionalComment=f"(m für alle Gelenke)", plotSaveDir=plotSaveDir)
+    config_f: BAP_config = BAP_config(sets=sets_f, colors=iter(colors_joints), dataName1=dataName1, dataName2=dataName2,
+                                      units="[N]", additionalComment=f"(f für alle Gelenke)", plotSaveDir=plotSaveDir)
+
+    # Plotten
+    generate_bland_altman_plot(config=config_m, showplot=False)
+    generate_bland_altman_plot(config=config_f, showplot=False)
 
     return
 
@@ -74,7 +159,7 @@ def create_joints_spatial_force_list(topic_fp: str, topic_jp: str, msgs_compound
     return joints_spatial_force_list
 
 
-def force_to_joint_plot(joints_spatial_force_list: "list[Joints_spatial_force]", plotSaveDir: str):
+def force_to_joint(joints_spatial_force_list: "list[Joints_spatial_force]", plotSaveDir: str) -> "Tuple[list[list[list[float]]], list[list[list[float]]]]":
     # m{x, y, z}: Nm
     # f{x, y, z}: N
     # Joint{i}: (mx, my, mz, fx, mf, fz)
@@ -108,6 +193,14 @@ def force_to_joint_plot(joints_spatial_force_list: "list[Joints_spatial_force]",
 
                 top_down__force_components__to__joints__to__datapoints[force_component_index][joint_index].append(joint_force_top_down)
 
+    # End: force_to_joint
+
+    return bottom_up__force_components__to__joints__to__datapoints, top_down__force_components__to__joints__to__datapoints
+
+
+def force_to_joint_plot(joints_spatial_force_list: "list[Joints_spatial_force]", plotSaveDir: str):
+    bottom_up__force_components__to__joints__to__datapoints, top_down__force_components__to__joints__to__datapoints = force_to_joint(joints_spatial_force_list, plotSaveDir)
+
     force_component__to__sets_joints_to_datapoints: list[list[BAP_set]] = list()
     for force_component_index in range(6):
         sets_joint_to_datapoints: list[BAP_set] = list()
@@ -119,13 +212,13 @@ def force_to_joint_plot(joints_spatial_force_list: "list[Joints_spatial_force]",
             sets_joint_to_datapoints.append(BAP_set(x1=bottom_up__joint__to__datapoints, x2=top_down__joint__to__datapoints))
         force_component__to__sets_joints_to_datapoints.append(sets_joint_to_datapoints)
 
-    # End: force_to_joint
-
     colors_joints = list()
     colors_joints.extend(["b", "g", "r", "c", "m", "y"])
 
     forces_names: list[str] = ["mx", "my", "mz", "fx", "fy", "fz"]
     forces_units: list[str] = ["Nm", "Nm", "Nm", "N", "N", "N"]
+
+    plotSaveDir = f"{plotSaveDir}force_to_joint/"
 
     for force_component_index in range(6):
         force_name: str = forces_names[force_component_index]
