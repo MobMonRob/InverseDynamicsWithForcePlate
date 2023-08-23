e533c6ab7983265ac3d6b1600338ecf7416e302d
deralbert <kurt.goedel@protonmail.com>
2023-08-23 19:03:54 +0200

    Update BAPs.

--- Scripts/Pipelines/BAP__CoP_force_plate__CoP_chisel_tip_marker.py
+++ Scripts/Pipelines/BAP__CoP_force_plate__CoP_chisel_tip_marker.py
@@ -48,16 +48,16 @@ class BAP__CoP_force_plate__CoP_chisel_tip_marker:
         colors.extend(["b"] * 4)
         colors.extend(["g"] * 2)
 
-        while True:
-            dataName1 = "CoP Kraftmessplatte"
-            dataName2 = "CoP Marker an der Spitze"
-            units = "[mm]"
-            config_x: BAP_config = BAP_config(sets=sets_x, colors=iter(colors), dataName1=dataName1, dataName2=dataName2, units=units, additionalComment="(x-Achse)", plotSaveDir=self.plotSaveDir)
-            config_y: BAP_config = BAP_config(sets=sets_y, colors=iter(colors), dataName1=dataName1, dataName2=dataName2, units=units, additionalComment="(y-Achse)", plotSaveDir=self.plotSaveDir)
+        # while True:
+        dataName1 = "CoP Kraftmessplatte"
+        dataName2 = "CoP Marker an der Spitze"
+        units = "[mm]"
+        config_x: BAP_config = BAP_config(sets=sets_x, colors=iter(colors), dataName1=dataName1, dataName2=dataName2, units=units, additionalComment="(x-Achse)", plotSaveDir=self.plotSaveDir)
+        config_y: BAP_config = BAP_config(sets=sets_y, colors=iter(colors), dataName1=dataName1, dataName2=dataName2, units=units, additionalComment="(y-Achse)", plotSaveDir=self.plotSaveDir)
 
-            # Plotten
-            generate_bland_altman_plot(config=config_x, showplot=True)
-            generate_bland_altman_plot(config=config_y, showplot=False)
+        # Plotten
+        generate_bland_altman_plot(config=config_x)
+        generate_bland_altman_plot(config=config_y)
 
         return
 
